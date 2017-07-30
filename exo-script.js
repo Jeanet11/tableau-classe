f92c95e82c118641afc1e5eec36cc9947600dcc1
Jeanne <jeanne.tiramani@hotmail.fr>
2017-07-30 12:22:25 +0200

    exercices 1 à 5

---
+++ exo-script.js
@@ -0,0 +1,109 @@
+var classe1 = [
+{ nom: 'Pierre', age: 40, sexe: 'M' },
+{ nom: 'Marie', age: 18, sexe: 'F' },
+{ nom: 'Louise', age: 16 , sexe: 'F' },
+{ nom: 'Damien', age: 55, sexe: 'M' },
+{ nom: 'Louis', age: 19, sexe: 'M' }
+];
+
+var classe2 = [
+{ nom: 'Elodie', age: 27, sexe: 'F' },
+{ nom: 'Christophe', age: 48, sexe: 'M' },
+{ nom: 'Aurelie', age: 32, sexe: 'F' },
+{ nom: 'Jeanne', age: 15, sexe: 'F' },
+{ nom: 'Martin', age: 77, sexe: 'M' },
+{ nom: 'Eric', age: 18, sexe: 'M' },
+{ nom: 'Jacques', age: 35, sexe: 'M' },
+{ nom: 'Eric', age: 47, sexe: 'M' }
+];
+
+// calcul du nombre d'hommes par
+function nombreHommes(classe) {
+	var nbHomme = 0;
+
+	for(i = 0; i < classe.length; i++) {
+		if('M' === classe[i].sexe ) {
+		  nbHomme++;
+		}
+	}
+
+	return nbHomme;
+}
+
+var nbHommes = nombreHommes(classe1);
+console.log("Il y a " + nbHommes + " hommes dans la classe.");
+
+
+// calcul du pourcentage d'hommes
+function proportionHomme(classe) {
+	var nombreTotal= classe.length;
+	var nbHommes = nombreHommes(classe);
+	var proportion= nbHommes/nombreTotal * 100;
+
+	return proportion;
+	
+  }
+
+console.log("Il y a " + proportionHomme(classe2) + "% d'hommes dans la classe.");
+
+
+// calcul proportion femmes
+function proportionFemme(classe) {
+	var propHomme = proportionHomme(classe);
+	var propFemme = 100 - propHomme;
+	console.log("Il y a " + propFemme + "% de femmes dans la classe.");
+}
+proportionFemme(classe1);
+
+
+ //Définissez une fonction moyenneAge( classe ) qui renvoie la moyenne d'age de la classe.
+function moyenneAge(classe) {
+ 	var sommeAges = 0;
+
+ 	for(var i = 0, c = classe.length; i < c; i++) {
+  		sommeAges = sommeAges + (classe[i].age);
+ 	}
+
+    var moyenne = sommeAges / c;
+ 	console.log("La moyenne d'âge de la classe est " +  moyenne + " ans.");
+ }
+
+ moyenneAge(classe1);
+
+
+ // moyenne d'age par sexe
+ /*function moyenneAge(classe, sexe = 'T') {
+
+ 	for ("M" in classe) {
+ 		var sommeAges = classe[];
+
+ 	if (sexe === 'F') {
+ 		sommeAges = 
+
+
+ 	}
+ }
+
+moyenneAge(classe1, 'F');
+
+*/
+/*
+function somme(classe) {
+	var plus = classe.reduce
+*/
+/*
+var classe0 = [
+'Pierre',
+'Jacques',
+'Marie',
+'Louis'
+];
+
+function displayClasse(classe) {
+	
+	for(i = 0; i < classe.length; i++) {
+		console.log(classe[i]);
+	}
+}
+displayClasse(classe0);
+*/
